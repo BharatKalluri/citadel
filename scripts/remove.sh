202f8b61127d1f91898a9b779ef847d2e8bba84b
Bharat Kalluri <bharatkalluri@protonmail.com>
2024-08-13 00:37:47 +0530

    move to env vars

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -3,17 +3,17 @@
 set -e
 
 # verify the app exists in installed_apps
-if [ ! -d "$CITADEL_ROOT_DIR/installed_apps/$1" ]; then
+if [ ! -d "$INSTALLED_APPS_DIR/$1" ]; then
     echo "App $1 not found in installed_apps!"
     exit 1
 fi
 
 # docker compose down if the app is running
-cd "$CITADEL_ROOT_DIR/installed_apps/$1"
+cd "$INSTALLED_APPS_DIR/$1"
 
 docker compose down
 
 # remove the app from installed_apps
-rm -rf "$CITADEL_ROOT_DIR/installed_apps/$1"
+rm -rf "$INSTALLED_APPS_DIR/$1"
 
 echo "Removed $1!"
